10d32b31d48695bd308d22cef4827507637532d5
Jeremiah Sturgill <github@contact.jeremiahsturgill.com>
2021-08-24 08:59:55 -0700

    updated test data
    - new templates for translation functions
    - fixed Golden Masters to properly reflect expected sprintf generation
    - fixed .gitignore files

--- jds-demo-plugin/tests/_data/cache/gettext/complex-test-template.php
+++
@@ -1,7 +0,0 @@
-<?php
-// translators: informal greeting
-sprintf( __( "Hello %s", "jds-demo-plugin-domain"), $name);
-
-sprintf( __( "%s", "jds-demo-plugin-domain"), $foobar);
-
-sprintf( __( "another %s", "jds-demo-plugin-domain"), "NOT A VARIABLE");
