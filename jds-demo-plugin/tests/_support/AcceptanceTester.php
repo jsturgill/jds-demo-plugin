c4afffababfe6d991a6612fed0b7d1eb2287ad74
Jeremiah Sturgill <github@contact.jeremiahsturgill.com>
2021-10-01 21:44:13 -0700

    moved authentication logic to actor

--- jds-demo-plugin/tests/_support/AcceptanceTester.php
+++ jds-demo-plugin/tests/_support/AcceptanceTester.php
@@ -2,7 +2,6 @@
 
 use Codeception\Actor;
 
-
 /**
  * Inherited Methods
  * @method void wantToTest($text)
@@ -22,7 +21,33 @@ class AcceptanceTester extends Actor
 {
     use _generated\AcceptanceTesterActions;
 
-    /**
-     * Define custom actions here
-     */
+    public function login($name, $password)
+    {
+        $I = $this;
+        // if snapshot exists - skipping login
+        if ($I->loadSessionSnapshot('login')) {
+            return;
+        }
+
+        $timeout = 10;
+
+        // load the login page
+        $I->amOnPage("/wp-login.php");
+        $I->waitForElement('#user_login', $timeout);
+        $I->waitForElement('#user_pass', $timeout);
+        $I->waitForElement('#wp-submit', $timeout);
+
+        // submit the form
+        $I->fillField('#user_login', $name);
+        $I->fillField('#user_pass', $password);
+        $I->click('#wp-submit');
+
+        // verify the things
+        $I->amOnPage("/wp-admin/");
+        $I->see("Welcome to WordPress!");
+        $I->see("Dashboard");
+
+        // saving snapshot
+        $I->saveSessionSnapshot('login');
+    }
 }
