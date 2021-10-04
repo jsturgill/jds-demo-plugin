5a7cd574a980209d4e4cd96c9ba81f92d7809a1d
Jeremiah Sturgill <github@contact.jeremiahsturgill.com>
2021-10-03 20:26:25 -0700

    fixed .pot file generation
    after some experimentation, `--include` and `--exclude` seem to not be
    working as documented
    (https://developer.wordpress.org/cli/commands/i18n/make-pot/).
    
    The twig template .pot file is extracted separately, and then merged
    with the plugin source .pot file.
    
    Additional tweaks may be required after i10n is actually attempted.

--- refresh-pot.sh
+++ refresh-pot.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR=$(pwd)
 
 printf "executing within %s \n\n" "$SCRIPT_DIR"
 
-php ./dev-utils/wp-cli.phar i18n make-pot ./jds-demo-plugin/src --ignore-domain ./jds-demo-plugin/languages/jds-demo-plugin.pot --include=./jds-demo-plugin/cache/gettext/*.php --exclude=./jds-demo-plugin/tests/* --merge
+php ./dev-utils/wp-cli.phar i18n make-pot ./jds-demo-plugin/cache/gettext jds-demo-plugin/cache/gettext/templates.pot --ignore-domain
+php ./dev-utils/wp-cli.phar i18n make-pot ./jds-demo-plugin/src jds-demo-plugin/languages/jds-demo-plugin.pot --merge=jds-demo-plugin/cache/gettext/templates.pot --ignore-domain
 
 printf "\njds-demo-plugin/languages/jds-demo-plugin.pot updated"
